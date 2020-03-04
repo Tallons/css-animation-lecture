c2a924e35801bd8a12205cc8f256ed69382e3acd
Tallons <Tallons27@yahoo.com>
2020-03-04 11:54:00 -0700

    activity completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from "./logo.svg"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      toggleDropdown: false
+    }
+  }
+  render(){
+
+    return (
+      <div className="App">
+      <header className="main-header">
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Stitch_%28Lilo_%26_Stitch%29.svg/1200px-Stitch_%28Lilo_%26_Stitch%29.svg.png" alt="stitch logo" className="header-logo" />
+        {this.state.toggleDropdown
+        ? (
+          <>
+          <span className="dropdown-toggle" onClick={() => this.setState({toggleDropdown: !this.state.toggleDropdown})}> ≡ </span>
+          <nav className ="mobile-links">
+            <p>Home</p>
+            <p>About</p>
+            <p>Contact</p>
+          </nav>
+          </>
+        )
+        :(
+          <span className="dropdown-toggle" onClick={() => this.setState({toggleDropdown: !this.state.toggleDropdown})}> ≡ </span>
+        )
+        }
+        <nav className="header-links">
+          <span>Home</span>
+          <span>About</span>
+          <span>Contract</span>
+        </nav>
       </header>
+      <img src={logo} alt='react.js logo' className='react-logo'/>
+      <img src={logo} alt='react.js logo' className='react-logo1'/>
+      <img src={logo} alt='react.js logo' className='react-logo2'/>
     </div>
-  );
+   );
+  }
 }
 
 export default App;
